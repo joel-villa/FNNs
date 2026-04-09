2d5c7104bfcd248d389eac3120dc876c0ec34639
Joel Villarreal II <joel.villa2329@gmail.com>
2026-04-09 14:44:59 -0600

    plots for 4

--- plots.py
+++ plots.py
@@ -1,25 +1,48 @@
 import matplotlib.pyplot as plt
 
 dropout = [20, 30, 40, 50]
-time = [15.4, 15.3, 15.2, 15.2]
-accuracy = [89.6, 89.4, 89.4, 89.1]
+time_single = [15.4, 15.3, 15.2, 15.2]
+accuracy_single = [89.6, 89.4, 89.4, 89.1]
+
+time_eight = [123.3, 122.6, 122.7, 122.5]
+accuracy_eight = [89.6, 89.5, 89.4, 89.2]
 
 # Plot 1: Time vs Dropout
 plt.figure()
-plt.plot(dropout, time, marker='o')
+plt.plot(dropout, time_single, marker='o')
 plt.xlabel("Dropout Probability (%)")
 plt.ylabel("Time (s)")
-plt.title("Training Time vs Dropout Probability")
+plt.title("Training Time vs Dropout Probability (Single FNN)")
 plt.grid(True)
-plt.savefig("plots/dropout_time.jpg")
+plt.savefig("plots/dropout_single_time.jpg")
 plt.show()
 
 # Plot 2: Accuracy vs Dropout
 plt.figure()
-plt.plot(dropout, accuracy, marker='o')
+plt.plot(dropout, accuracy_single, marker='o')
 plt.xlabel("Dropout Probability (%)")
 plt.ylabel("Test Accuracy (%)")
-plt.title("Test Accuracy vs Dropout Probability")
+plt.title("Test Accuracy vs Dropout Probability (Single FNN)")
 plt.grid(True)
-plt.savefig("plots/dropout_acc.jpg")
+plt.savefig("plots/dropout_single_acc.jpg")
+plt.show()
+
+# Plot 3: Time vs Dropout
+plt.figure()
+plt.plot(dropout, time_eight, marker='o')
+plt.xlabel("Dropout Probability (%)")
+plt.ylabel("Time (s)")
+plt.title("Training Time vs Dropout Probability (Eight FNNs)")
+plt.grid(True)
+plt.savefig("plots/dropout_eight_time.jpg")
+plt.show()
+
+# Plot 4: Accuracy vs Dropout
+plt.figure()
+plt.plot(dropout, accuracy_eight, marker='o')
+plt.xlabel("Dropout Probability (%)")
+plt.ylabel("Test Accuracy (%)")
+plt.title("Test Accuracy vs Dropout Probability (Eight FNNs)")
+plt.grid(True)
+plt.savefig("plots/dropout_eight_acc.jpg")
 plt.show()
